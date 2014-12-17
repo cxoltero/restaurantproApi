94541c517adb34c3dd5dff03631af3f82ef0b4cf
Roberto Hernandez <carlosh95@gmail.com>
2014-12-16 21:59:03 -0500

    fixed tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,27 @@
 class TasksController < ApplicationController
+	def create
+		@task = Task.new(task_params)
+		if @task.save
+			render json: @task, status: :created, location: @task
+		else
+			render json: @task.errors, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+
+		head :no_content
+	end
+
+	private
+		def find_task
+			@task = Task.find(params[:id])
+		end
+
+		def task_params
+			params.require(:task).permit(:title)
+	end
+
 end
