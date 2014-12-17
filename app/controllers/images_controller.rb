4892fd7ff4b7825a5f3faa19dbb31a0cec88c49c
Roberto Hernandez <carlosh95@gmail.com>
2014-12-16 22:19:01 -0500

    fixed amazon and images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,19 @@
 class ImagesController < ApplicationController
+ def index
+		@images = Image.all
+	end
+	def show
+		@image = Image.find(params[:id])
+	end
+	def create
+		@image = Image.create(image_params)
+	end
+	def destroy
+		@image = Image.find(params[:id])
+		@image.destroy
+	end
+private
+def image_params
+	params.require(:image).permit(:url)
+end
 end
