139b4adef9a12f10c87c25aa6af5b0398a6f55b0
Roberto Hernandez <carlosh95@gmail.com>
2014-12-16 21:50:45 -0500

    recipes controller is ready

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,55 @@
 class RecipesController < ApplicationController
+before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+def index
+	@recipes = Recipe.all.order("created_at DESC")
+	render json: @recipes
+end
+
+def new
+	@recipe = Recipe.new
+	render json: @recipe
+end
+
+def show
+	render json: @recipe
+end
+
+def create
+	@recipe = Recipe.new(recipe_params)
+	if @recipe.save
+		render json: @recipe, status: :created, location: @recipe
+	else
+		render json: @recipe.errors, status: :unprocessable_entity
+	end
+end
+
+def edit
+end
+
+def update
+	if @recipe.update(recipe_params)
+		render json: @recipe
+	else
+		render json: @recipe.errors, status: :unprocessable_entity
+	end
+	head :no_content
+end
+
+def destroy
+	@recipe.destroy
+	redirect_to root_path, notice: "Succesfully deleted recipe!"
+	head :no_content
+end
+
+
+private
+def find_recipe
+	@recipe = Recipe.find(params[:id])
+end
+
+def recipe_params
+	params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+end
+
 end
